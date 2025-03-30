15f8fb5ad3306d78521b3e963e6351893d226cc8
Svetlana Bazhenova <varta.73@mail.ru>
2025-03-30 20:26:27 +0300

    35.1

--- Dockerfile
+++ Dockerfile
@@ -2,8 +2,15 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
-COPY /requirements.txt /
+RUN apt-get update \
+  && apt-get install -y gcc libpg-dev \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/\*
 
-RUN pip install -r /requirements.txt --no-cache-dir
+COPY requirements.txt .
 
-COPY . ./
+RUN pip install --no-cache-dir -r requirements.txt
+
+COPY . .
+
+EXPOSE 8000
--- config/settings.py
+++ config/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 INSTALLED_APPS = [
@@ -111,6 +111,7 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR,  "staticfiles")
 
 
 MEDIA_URL = "/media/"
@@ -119,6 +120,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "users.User"
+
+CACHES = {
+    "default": {
+        "BACKEND": "django.core.cache.backends.redis.RedisCache",
+        "LOCATION": "redis://redis:6379/1",
+    }
+}
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=15),
